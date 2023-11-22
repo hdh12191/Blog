3363ed7892eae2e4f394ab1f30497ff7cf72ebcf
hdh12191 <hdh12191@naver.com>
2023-11-23 02:53:46 +0900

    blog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [title, setTitle] = useState([
+    "남자 코트 추천",
+    " 남자 니트 추천",
+    "남자 단화 추천",
+  ]); //state 변경 시 html 자동으로 재렌더링.
+  let [thumbsUpCount, setThumbsUpCount] = useState([0, 0, 0]);
+  let [modal, setModal] = useState(false);
+  let [modalTitle, setModalTitle] = useState(0);
+
+  let today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1;
+  let date = today.getDate();
+
+  let changeTitle = () => {
+    let copyTitle = [...title];
+    copyTitle[0] = "여자 코트 추천";
+    setTitle(copyTitle);
+  };
+
+  let sortKorean = () => {
+    let copyTitle = [...title];
+    copyTitle.sort();
+    setTitle(copyTitle);
+  };
+
+  let showModal = () => {
+    if (modal === false) {
+      setModal(true);
+    } else {
+      setModal(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>Dong's Blog</h4>
+      </div>
+      {title.map((a, i) => {
+        return (
+          <div className="list" key={i}>
+            <h4
+              onClick={() => {
+                showModal();
+                setModalTitle(i);
+              }}
+            >
+              {title[i]}
+            </h4>
+            <button onClick={sortKorean}> 정렬 </button>
+            <button
+              onClick={() => {
+                let copyThumbsUp = [...thumbsUpCount];
+                copyThumbsUp[i] = copyThumbsUp[i] + 1;
+                setThumbsUpCount(copyThumbsUp);
+              }}
+            >
+              👍
+            </button>
+            {thumbsUpCount[i]}
+
+            <p>{`${year}년${month}월${date}일`}</p>
+          </div>
+        );
+      })}
+
+      {modal === false ? null : <Modal title={title} modalTitle={modalTitle} year={year} month={month} date={date}/>}
     </div>
   );
+}
+
+function Modal(props) {
+  
+  return (
+    <div className="modal">
+      <h4>{props.title[props.modalTitle]}</h4>
+      <p>{`${props.year}년${props.month}월${props.date}일`}</p>
+      <p>상세내용</p>
+      <button>제목변경</button>
+    </div>
+  ); //컴포넌트 만들면 좋을때
+  //1.반복적인 html 축약할때.
+  //2.큰 페이지들.
+  //3.자주변경되는 것들.
 }
 
 export default App;
